bd1520c0acc9a0d32f715fbe3eed6c3b1a6e92a4
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-08 12:27:06 +0530

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,9 +1,6 @@
 import logging
 
-def setup_logger(name: str, log_file: str, level=logging.INFO) -> logging.Logger:
-    """
-    Set up a logger with the specified name, log file, and logging level.
-    """
+def setup_logger(name: str, log_file: str, level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
     handler = logging.FileHandler(log_file)
