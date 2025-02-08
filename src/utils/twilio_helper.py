c45df4b0509ea39622bbc36934e85dbe1f01c20f
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-08 12:27:17 +0530

    Update twilio_helper.py

--- src/utils/twilio_helper.py
+++ src/utils/twilio_helper.py
@@ -2,13 +2,6 @@ from twilio.rest import Client
 from src.config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER
 
 def send_sms(to_number: str, message: str):
-    """
-    Send an SMS using Twilio API.
-    """
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    message = client.messages.create(
-        body=message,
-        from_=TWILIO_PHONE_NUMBER,
-        to=to_number
-    )
+    message = client.messages.create(body=message, from_=TWILIO_PHONE_NUMBER, to=to_number)
     return message.sid
