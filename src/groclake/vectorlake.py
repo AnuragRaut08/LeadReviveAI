d61b09f2655e5964b25876d02de013c588d1ce06
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-08 12:26:36 +0530

    Update vectorlake.py

--- src/groclake/vectorlake.py
+++ src/groclake/vectorlake.py
@@ -2,16 +2,10 @@ from .authentication import authenticate
 
 client = authenticate()
 
-def create_lead_embedding(lead_data: dict) -> dict:
-    """
-    Create and store lead data embeddings using Groclake's Vectorlake.
-    """
+def create_lead_embedding(lead_data: dict):
     embedding = client.vectorlake.create_embedding(data=lead_data)
     client.vectorlake.store_embedding(embedding_id=lead_data["name"], embedding=embedding)
     return embedding
 
-def get_similar_leads(lead_name: str, top_k: int = 5) -> list:
-    """
-    Retrieve similar leads from Groclake's Vectorlake.
-    """
+def get_similar_leads(lead_name: str, top_k: int = 5):
     return client.vectorlake.query_embedding(embedding_id=lead_name, top_k=top_k)
